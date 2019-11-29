a7ecaf5058b9084f8329dbecbbe987ed82345860
Alexandra Pestruyeva <apestruy@gmail.com>
2019-11-29 02:41:07 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -31,7 +31,7 @@ def total_even_pairs(src)
   while row_index < src.count do 
     element0 = src[row_index][0]
     element1 = src[row_index][1]
-    if element0 % 2 == 0 && element1 % 2 == 0
+    if (element0 % 2 == 0 && element1 % 2 == 0)
       results = (element0 + element1)
     total += results
   end
